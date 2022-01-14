09b8c63ae674426f50193e7aa7741113f7d275a3
james1mumo <james1mumo@gmail.com>
2022-01-14 16:02:26 +0300

    Admin can add Users and Items

--- gearbox_academy_logistics.sql
+++ gearbox_academy_logistics.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 13, 2022 at 01:59 PM
+-- Generation Time: Jan 14, 2022 at 03:00 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -25,6 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `second_name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `first_name`, `second_name`, `email`, `password`) VALUES
+(1, 'Ephy', 'Ngugi', 'admin@gmail.com', 'admin'),
+(2, 'Foo', 'bar', 'foo@gmail.com', 'foobar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `shelf` int NOT NULL,
+  `drawer` int NOT NULL,
+  `quantity` int NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `external_link` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `image`, `shelf`, `drawer`, `quantity`, `description`, `external_link`) VALUES
+(2, 'resistors', 'profile.jpg', 2, 3, 23, '23', '33'),
+(8, 'a', 'items/a-0.jpg', 2, 2, 2, '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,6 +80,7 @@ CREATE TABLE `users` (
   `first_name` varchar(20) NOT NULL,
   `second_name` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
   `profile_pic` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -41,12 +89,26 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `second_name`, `email`, `profile_pic`, `password`) VALUES
-(1, 'ruth', 'doe', 'ruthdoe@gmail.com', '.', 'ruth');
+INSERT INTO `users` (`id`, `first_name`, `second_name`, `email`, `mobile`, `profile_pic`, `password`) VALUES
+(1, 'ruth', 'doe', 'ruthdoe@gmail.com', NULL, 'profile.jpg', 'ruth'),
+(3, 'foo', 'bar', 'foo@gmail.com', NULL, 'profile.jpg', 'foo'),
+(4, 'james', 'mumo', 'jmumo@gmail.com', NULL, 'profile.jpg', 'james');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -59,10 +121,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 import flask
 import mysql.connector
+import bcrypt
+import os
 
 DATABASE_USER = 'phpmyadmin'
 DATABASE_PASSWORD = '123456'
 DATABASE_HOST = 'localhost'
 DATABASE_NAME = 'gearbox_academy_logistics'
+
+IMAGES_DIRECTORY = './static/images/'
 
 app = Flask(__name__)
 app.secret_key = "jiugyfhafd76e48w3yrgeurbgi7846e7rt"
@@ -16,7 +20,7 @@ my_db = mysql.connector.connect(
     password=DATABASE_PASSWORD,
     database=DATABASE_NAME
 )
-my_cursor = my_db.cursor()
+my_cursor = my_db.cursor(dictionary=True)
 
 
 @app.route('/', methods=['GET'])
@@ -37,15 +41,12 @@ def login():
         result = my_cursor.fetchall()
 
         if len(result) == 0:
-            return 'Invalid username or password. <a href="/login">Go back?</a>'
+            flask.session['error-message'] = 'Invalid email or password.'
+            return flask.render_template('login.html')
         else:
-            # here user has logged in
+            # here user has logged in successfully
             flask.session['logged_in'] = True
             return flask.redirect(flask.url_for('index'))
-
-
-
-
 
     else:
         return flask.render_template('login.html')
@@ -53,9 +54,153 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    if flask.session.get('logged_in'):
-        flask.session.clear()
-    return flask.redirect(flask.url_for('login'))
+    flask.session.clear()
+    return flask.redirect(flask.url_for('index'))
+
+
+#
+# Admin section begins here
+#
+
+@app.route('/admin', methods=['GET'])
+def admin():
+    if not flask.session.get('admin_logged_in'):
+        return flask.redirect(flask.url_for('admin_login'))
+    return flask.render_template('admin/home.html')
+
+
+@app.route('/admin-login', methods=['GET', 'POST'])
+def admin_login():
+    if 'submit' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+
+        my_cursor.execute(f'SELECT * FROM admin WHERE email=\'{email}\' AND password=\'{password}\'')
+
+        result = my_cursor.fetchall()
+
+        if len(result) == 0:
+            flask.session['error-message'] = 'Invalid email or password.'
+            return flask.render_template('admin/login.html')
+
+        else:
+            # here Admin has logged in successfully
+            flask.session['admin_logged_in'] = True
+            return flask.redirect(flask.url_for('admin'))
+
+    else:
+        return flask.render_template('admin/login.html')
+
+
+#
+# Admin management section
+#
+
+@app.route('/manage-users', methods=['GET', 'POST'])
+def manage_users():
+    # first check if admin is logged
+    if not flask.session.get('admin_logged_in'):
+        return flask.redirect(flask.url_for('admin_login'))
+
+    if 'add-user' in request.form:
+        first_name = request.form['firstname']
+        second_name = request.form['lastname']
+        email = request.form['email']
+        mobile = request.form['mobile']
+
+        # here is where we hash our passwords to store them securely
+        hashed_password = bcrypt.hashpw(email.encode('utf-8'), bcrypt.gensalt())
+
+        my_cursor.execute('INSERT INTO users (first_name, second_name, email, mobile, profile_pic, password) '
+                          'VALUES (%s,%s,%s,%s,"profile.jpg",%s)',
+                          (first_name, second_name, email, mobile, hashed_password))
+        my_db.commit()
+
+        flask.session['success-message'] = f'User {first_name} {second_name} added successfully'
+    elif 'delete-user' in request.form:
+        user_id = request.form['id']
+
+        my_cursor.execute('DELETE FROM users WHERE id = %s', (user_id,))
+        my_db.commit()
+
+        flask.session['success-message'] = f'User Deleted successfully'
+    elif 'get-row' in request.form:
+        user_id = request.form['id']
+        my_cursor.execute('SELECT id, first_name, second_name FROM users WHERE id = %s', (user_id,))
+        user = my_cursor.fetchone()
+        return flask.jsonify(user)
+
+    my_cursor.execute(f'SELECT * FROM users')
+    users = my_cursor.fetchall()
+
+    return flask.render_template('admin/manage-users.html', users=users)
+
+
+@app.route('/manage-items', methods=['GET', 'POST'])
+def manage_items():
+    # first check if admin is logged in
+    if not flask.session.get('admin_logged_in'):
+        return flask.redirect(flask.url_for('admin_login'))
+
+    if 'add-item' in request.form:
+        name = request.form['name']
+        shelf = request.form['shelf']
+        drawer = request.form['drawer']
+        quantity = request.form['quantity']
+        description = request.form['description']
+        link = request.form['link']
+        if request.files['photo'].filename == '':
+            image_path = 'profile.jpg'
+        else:
+            photo = request.files['photo']
+            image_path = f'items/{name}-{photo.filename}'
+            photo.save(os.path.join(IMAGES_DIRECTORY, image_path))
+
+        my_cursor.execute('INSERT INTO items (name, image, shelf, drawer, quantity, description, external_link) '
+                          'VALUES (%s,%s,%s,%s,%s,%s, %s)',
+                          (name, image_path, shelf, drawer, quantity, description, link))
+        my_db.commit()
+
+        flask.session['success-message'] = f'Item {name} added successfully'
+    elif 'edit-item' in request.form:
+        item_id = request.form['id']
+        name = request.form['name']
+        shelf = request.form['shelf']
+        drawer = request.form['drawer']
+        quantity = request.form['quantity']
+        description = request.form['description']
+        link = request.form['link']
+        if request.files['photo'].filename == '':
+            image_path = 'profile.jpg'
+        else:
+            photo = request.files['photo']
+            image_path = f'items/{name}-{photo.filename}'
+            photo.save(os.path.join(IMAGES_DIRECTORY, image_path))
+
+        my_cursor.execute('UPDATE items SET name=%s, image=%s, shelf=%s, drawer=%s, quantity=%s, description=%s, '
+                          'external_link=%s '
+                          'WHERE id = %s', (name, image_path, shelf, drawer, quantity, description, link, item_id))
+        my_db.commit()
+
+        flask.session['success-message'] = f'Item {name} updated successfully'
+
+    elif 'delete-item' in request.form:
+        user_id = request.form['id']
+
+        my_cursor.execute('DELETE FROM items WHERE id = %s', (user_id,))
+        my_db.commit()
+
+        flask.session['success-message'] = f'Item Deleted successfully'
+    elif 'get-row' in request.form:
+        user_id = request.form['id']
+        my_cursor.execute('SELECT * FROM items WHERE id = %s', (user_id,))
+        user = my_cursor.fetchone()
+        return flask.jsonify(user)
+
+    my_cursor.execute(f'SELECT * FROM items')
+    items = my_cursor.fetchall()
+
+    return flask.render_template('admin/manage-items.html', items=items)
 
 
 if __name__ == '__main__':
