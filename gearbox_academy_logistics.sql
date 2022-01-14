4706455d1be1096a9fe5060bdfdc7cf59c40f2aa
james1mumo <james1mumo@gmail.com>
2022-01-15 00:59:29 +0300

    Users can search items and choose the ones they want to pick from the shelves

--- gearbox_academy_logistics.sql
+++ gearbox_academy_logistics.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 14, 2022 at 03:00 PM
+-- Generation Time: Jan 14, 2022 at 11:58 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -47,6 +47,19 @@ INSERT INTO `admin` (`id`, `first_name`, `second_name`, `email`, `password`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `item_id` int NOT NULL,
+  `quantity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `items`
 --
 
@@ -66,8 +79,44 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `name`, `image`, `shelf`, `drawer`, `quantity`, `description`, `external_link`) VALUES
-(2, 'resistors', 'profile.jpg', 2, 3, 23, '23', '33'),
-(8, 'a', 'items/a-0.jpg', 2, 2, 2, '', '');
+(2, 'resistor', 'items/resistors-fg.jpeg', 2, 3, 23, '23', '33'),
+(8, 'keypad', 'items/keypad-8456845.jpeg', 2, 2, 2, '', ''),
+(9, 'led', 'items/led-8945.jpeg', 3, 1, 10, '', ''),
+(10, '8 x 8 led matrix', 'items/8 x 8 led matrix-5848.jpeg', 2, 1, 12, '', ''),
+(11, 'lcd display', 'items/lcd display-5455485.jpeg', 5, 1, 3, '', ''),
+(12, '7 segment display', 'items/7 segment display-rewtyui.jpeg', 2, 1, 10, '', ''),
+(13, 'potentiometer', 'items/potentiometer-4516584.jpeg', 2, 1, 1, '', ''),
+(14, 'piezo', 'items/piezo-54.jpeg', 1, 2, 10, '', ''),
+(15, 'capacitor', 'items/capacitor-84584.jpeg', 1, 2, 1, '', ''),
+(16, 'diode', 'items/diode-894515.jpeg', 2, 1, 10, '', ''),
+(17, 'zener diode', 'items/zener diode-84554.jpeg', 2, 1, 3, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `item_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`id`, `user_id`, `item_id`, `quantity`, `date_time`) VALUES
+(1, 1, 8, 1, '2022-01-14 21:41:51'),
+(2, 1, 2, 1, '2022-01-14 21:41:56'),
+(3, 1, 8, 1, '2022-01-14 21:43:46'),
+(4, 1, 2, 1, '2022-01-14 21:43:46'),
+(5, 1, 16, 1, '2022-01-14 21:44:34'),
+(6, 1, 15, 1, '2022-01-14 21:44:34'),
+(7, 1, 9, 1, '2022-01-14 21:44:34');
 
 -- --------------------------------------------------------
 
@@ -105,9 +154,21 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -127,10 +188,22 @@ ALTER TABLE `admin`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import flask
 import mysql.connector
 import bcrypt
 import os
+import json
 
 DATABASE_USER = 'phpmyadmin'
 DATABASE_PASSWORD = '123456'
@@ -30,6 +31,16 @@ def index():
     return flask.render_template('home.html')
 
 
+@app.route('/logout', methods=['GET'])
+def logout():
+    flask.session.clear()
+    return flask.redirect(flask.url_for('index'))
+
+
+#############################
+# User section begins here  #
+#############################
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'submit' in request.form:
@@ -46,21 +57,92 @@ def login():
         else:
             # here user has logged in successfully
             flask.session['logged_in'] = True
+            flask.session['user_id'] = result[0].get('id')
             return flask.redirect(flask.url_for('index'))
 
     else:
         return flask.render_template('login.html')
 
 
-@app.route('/logout', methods=['GET'])
-def logout():
-    flask.session.clear()
-    return flask.redirect(flask.url_for('index'))
+@app.route('/search-items', methods=['GET', 'POST'])
+def search_items():
+    # first check if user is logged in
+    if not flask.session.get('logged_in'):
+        return flask.redirect(flask.url_for('login'))
+
+    if 'add-item-to-cart' in request.form:
+        item_id = request.form['id']
+        user_id = flask.session.get('user_id')
+
+        my_cursor.execute('INSERT INTO cart (user_id, item_id, quantity) '
+                          'VALUES (%s,%s,"1")',
+                          (user_id, item_id))
+        my_db.commit()
+
+        my_cursor.execute('SELECT name FROM items WHERE id = %s', (item_id,))
+        result = my_cursor.fetchone()
+
+        return flask.jsonify(result)
+
+    my_cursor.execute(f'SELECT * FROM items')
+    items = my_cursor.fetchall()
+
+    return flask.render_template('search-items.html', items=items)
 
 
-#
-# Admin section begins here
-#
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    # first check if user is logged in
+    if not flask.session.get('logged_in'):
+        return flask.redirect(flask.url_for('login'))
+
+    user_id = flask.session.get('user_id')
+
+    if 'delete-item-from-cart' in request.form:
+        item_id = request.form['id']
+        my_cursor.execute('DELETE FROM cart WHERE user_id = %s AND item_id = %s', (user_id, item_id))
+        my_db.commit()
+
+        my_cursor.execute('SELECT id,name FROM items WHERE id = %s', (item_id,))
+        result = my_cursor.fetchone()
+
+        return flask.jsonify(result)
+    elif 'open-locks' in request.form:
+        items = request.form['items']
+
+        shelves_to_open = []
+
+        for item in json.loads(items):
+            my_cursor.execute('SELECT shelf,drawer FROM items WHERE id = %s', (item.get('id'),))
+            result = my_cursor.fetchone()
+            shelves_to_open.append(result.get('shelf'))
+
+            my_cursor.execute('INSERT INTO logs (user_id, item_id, quantity)'
+                              'VALUES (%s,%s,%s)', (user_id, item.get('id'), item.get('quantity')))
+            my_db.commit()
+
+        # here is where we should communicate with the nodeMcu to open locks
+        shelves_to_open = set(shelves_to_open)
+
+        # clear users cart items
+        my_cursor.execute('DELETE FROM cart WHERE user_id = %s ', (user_id,))
+        my_db.commit()
+
+    my_cursor.execute('SELECT * FROM cart WHERE user_id = %s', (user_id,))
+    results = my_cursor.fetchall()
+    items = []
+    # probably should use mysql join here
+    for result in results:
+        my_cursor.execute('SELECT * FROM items WHERE id = %s', (result.get('item_id'),))
+        items.append(my_cursor.fetchone())
+
+    return flask.render_template('checkout.html', items=items)
+
+
+#############################
+# Admin section begins here #
+#############################
+
 
 @app.route('/admin', methods=['GET'])
 def admin():
@@ -92,9 +174,9 @@ def admin_login():
         return flask.render_template('admin/login.html')
 
 
-#
-# Admin management section
-#
+############################
+# Admin management section #
+############################
 
 @app.route('/manage-users', methods=['GET', 'POST'])
 def manage_users():
@@ -171,15 +253,18 @@ def manage_items():
         description = request.form['description']
         link = request.form['link']
         if request.files['photo'].filename == '':
-            image_path = 'profile.jpg'
+            my_cursor.execute('UPDATE items SET name=%s, shelf=%s, drawer=%s, quantity=%s, description=%s, '
+                              'external_link=%s '
+                              'WHERE id = %s', (name, shelf, drawer, quantity, description, link, item_id))
         else:
             photo = request.files['photo']
             image_path = f'items/{name}-{photo.filename}'
             photo.save(os.path.join(IMAGES_DIRECTORY, image_path))
 
-        my_cursor.execute('UPDATE items SET name=%s, image=%s, shelf=%s, drawer=%s, quantity=%s, description=%s, '
-                          'external_link=%s '
-                          'WHERE id = %s', (name, image_path, shelf, drawer, quantity, description, link, item_id))
+            my_cursor.execute('UPDATE items SET name=%s, image=%s, shelf=%s, drawer=%s, quantity=%s, description=%s, '
+                              'external_link=%s '
+                              'WHERE id = %s', (name, image_path, shelf, drawer, quantity, description, link, item_id))
+
         my_db.commit()
 
         flask.session['success-message'] = f'Item {name} updated successfully'
