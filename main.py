78bb3ecdbc96462329195fa82f367f9c0f29b2ce
james1mumo <james1mumo@gmail.com>
2022-01-17 15:31:23 +0300

    Users passwords are stored securely by hashing them first

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import bcrypt  # for hashing passwords
 import os  # for saving uploaded images to file system
 import json  # for manipulating AJAX request data
 
-
 """
     Set your MySQL's password here
 """
@@ -115,21 +114,68 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        my_cursor.execute(f'SELECT * FROM users WHERE email=\'{email}\' AND password=\'{password}\'')
-
+        my_cursor.execute(f'SELECT * FROM users WHERE email=%s', (email,))
         result = my_cursor.fetchall()
-
         if len(result) == 0:
-            flask.session['error-message'] = 'Invalid email or password.'
+            flask.session['error-message'] = f'User with this email "{email}" does not exist'
             return flask.render_template('login.html')
-        else:
-            # here user has logged in successfully
-            flask.session['logged_in'] = True
-            flask.session['user_id'] = result[0].get('id')
-            return flask.redirect(flask.url_for('index'))
+
+        hashed_password = result[0].get('password')
+        if not bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+            flask.session['error-message'] = f'You Entered A Wrong Password'
+            return flask.render_template('login.html')
+
+        # here user has logged in successfully
+        flask.session['logged_in'] = True
+        flask.session['user_id'] = result[0].get('id')
+        flask.session['user'] = result[0]
+        return flask.redirect(flask.url_for('index'))
 
     else:
         return flask.render_template('login.html')
+
+
+@app.route('/edit-profile', methods=['GET', 'POST'])
+def edit_profile():
+    if not flask.session.get('logged_in'):
+        return flask.redirect(flask.url_for('login'))
+
+    user_id = flask.session.get('user_id')
+
+    if 'submit' in request.form:
+        first_name = request.form['firstname']
+        second_name = request.form['lastname']
+        mobile = request.form['mobile']
+
+        my_cursor.execute('UPDATE users SET first_name=%s, second_name=%s, mobile=%s WHERE id = %s',
+                          (first_name, second_name, mobile, user_id))
+        my_db.commit()
+
+        if not request.form['password'] == '':
+            # here is where we hash our passwords to store them securely
+            hashed_password = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            my_cursor.execute('UPDATE users SET password=%s WHERE id = %s', (hashed_password, user_id))
+            my_db.commit()
+
+        if not request.files['photo'].filename == '':
+            photo = request.files['photo']
+            image_path = f'users/{second_name}-{photo.filename}'
+            photo.save(os.path.join(IMAGES_DIRECTORY, image_path))
+
+            my_cursor.execute('UPDATE users SET profile_pic=%s WHERE id = %s', (image_path, user_id))
+            my_db.commit()
+
+        my_cursor.execute(f'SELECT * FROM users WHERE id=%s', (user_id,))
+        user = my_cursor.fetchone()
+        flask.session['user'] = user
+
+        flask.session['success-message'] = f'Details updated successfully'
+        return flask.redirect(flask.url_for('index'))
+
+    my_cursor.execute(f'SELECT * FROM users WHERE id=%s', (user_id,))
+    user = my_cursor.fetchone()
+
+    return flask.render_template('edit-profile.html', user=user)
 
 
 @app.route('/search-items', methods=['GET', 'POST'])
@@ -198,6 +244,11 @@ def checkout():
 
     my_cursor.execute('SELECT * FROM cart WHERE user_id = %s', (user_id,))
     results = my_cursor.fetchall()
+
+    if len(results) == 0:
+        flask.session['error-message'] = 'Choose some items first'
+        return flask.redirect(flask.url_for('search_items'))
+
     items = []
     # probably should use mysql join here
     for result in results:
@@ -357,4 +408,4 @@ def manage_items():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000, debug=True, host='0.0.0.0')
+    app.run(threaded=True, port=5000, debug=False, host='0.0.0.0')
